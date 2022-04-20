6ec457a852c587237e45ceecc549a3d06be89831
kazura2 <sobczakd2000@gmail.com>
2022-04-20 18:56:58 +0200

    v2

---
+++ main.tf
@@ -0,0 +1,41 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup"
+  location = "eastus"
+}
+
+resource "azurerm_app_service_plan" "appserviceplan" {
+  name                = "webappcloudappadministration"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "webapp" {
+  name                = "webappcloudappadministration"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
+  source_control {
+    repo_url           = "git@github.com:kazura2/cloud_app_administration.git"
+    branch             = "master"
+    manual_integration = true
+    use_mercurial      = false
+  }
+}
